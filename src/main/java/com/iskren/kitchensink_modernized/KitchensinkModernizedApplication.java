46e76e4edcf6a55da02a8c5e353be7782708b843
Iskren Vanev <iskren3009@gmail.com>
2026-04-04 00:48:08 +0300

    Initial commit - migrated kitchensink project

---
+++ src/main/java/com/iskren/kitchensink_modernized/KitchensinkModernizedApplication.java
@@ -0,0 +1,13 @@
+package com.iskren.kitchensink_modernized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkModernizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchensinkModernizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iskren/kitchensink_modernized/KitchensinkModernizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskren.kitchensink_modernized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkModernizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
